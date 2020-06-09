f317f0e7d926776e9c0343365949c3301f44cd41
Samuel Gleisner <samuel@magnet.cl>
2020-06-09 11:47:42 -0400

    style: correct whitespace on test_models.py

--- app/core/test_models.py
+++ app/core/test_models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with an email is successful"""
+        """Test creating a new user with an email is successful."""
         email = "test@samy.cl"
         password = "Djangorf123"
         user = get_user_model().objects.create_user(
@@ -16,7 +16,7 @@ class ModelTests(TestCase):
 
 
 def test_new_user_email_normalized(self):
-    """Test the email for a new user is normalized"""
+    """Test the email for a new user is normalized."""
     email = "test@SAMY.cl"
     user = get_user_model().objects.create_user(email, "test12345")
 
@@ -24,6 +24,6 @@ def test_new_user_email_normalized(self):
 
 
 def test_new_user_invalid_email(self):
-    """Test creating user with no email raises error"""
+    """Test creating user with no email raises error."""
     with self.assertRaises(ValueError):
-        get_user_model().objects.create_user(None, 'test123')
+        get_user_model().objects.create_user(None, 'test12345')
